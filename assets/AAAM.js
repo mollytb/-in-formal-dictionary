40968147b900fc5f9a0dc6a3e0afebd765e5586d
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-03-14 18:10:58 -0400

    shell and firebase

---
+++ assets/AAAM.js
@@ -0,0 +1,15 @@
+console.log(12);
+$(document).ready(function () {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD33QsSEZD9OzsUQz7B_c7Tet4MxMfixpo",
+    authDomain: "aaam-38e63.firebaseapp.com",
+    databaseURL: "https://aaam-38e63.firebaseio.com",
+    projectId: "aaam-38e63",
+    storageBucket: "",
+    messagingSenderId: "1074798895512"
+  };
+  firebase.initializeApp(config);
+
+});
