7b6b77d7ef3da5725f4fdc82b8dc03f1758d5f14
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-03-17 12:38:05 -0400

    ajax calls f^c__ing work

--- assets/AAAM.js
+++ assets/AAAM.js
@@ -1,18 +1,13 @@
 console.log(12);
 var oxfordCredentials = "2aa0189a"
+var oxKey = "ae54f1dd060d103e0947e768e0963bba"
 var oxfordBaseURL = "https://od-api.oxforddictionaries.com/api/v1"
 var urbanDictionaryKey = "e86PuErIxamshxX1NQoXzPi9BD9Sp1C1SgDjsnrfJoJR5BOft9"
 var urbanDictionaryBaseURL = "mashape-community-urban-dictionary.p.mashape.com"
 
 
 $(document).ready(function () {
-  //supposed node.js connect to urban dictionary
-  unirest.get("https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat")
-  .header("X-Mashape-Key", "e86PuErIxamshxX1NQoXzPi9BD9Sp1C1SgDjsnrfJoJR5BOft9")
-  .header("X-Mashape-Host", "mashape-community-urban-dictionary.p.mashape.com")
-  .end(function (result) {
-    console.log(result.status, result.headers, result.body);
-  });
+
 
   // Initialize Firebase
   var config = {
@@ -24,5 +19,27 @@ $(document).ready(function () {
     messagingSenderId: "1074798895512"
   };
   firebase.initializeApp(config);
+  $.ajax({
+    headers: {
+      accept: "application/json",
+      app_id: "2aa0189a",
+      app_key: "ae54f1dd060d103e0947e768e0963bba"
+    },
+    url: "https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/entries/en/&" + term,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
 
+  });
+ 
+  $.ajax({
+  url: "http://api.urbandictionary.com/v0/define?term=&" + term,
+  method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.list[0].definition);
+  });
+  
+  //ready document close function tokens
 });
