44018a84f56cd3db7f338ac94c962651b267211e
Case Western Reserve University <andrew.mancuso@case.edu>
2018-03-20 19:31:10 -0400

    added clear button, little more tidying

--- assets/javascript/AAAM.js
+++ assets/javascript/AAAM.js
@@ -5,7 +5,7 @@ var oxfordBaseURL = "https://od-api.oxforddictionaries.com/api/v1"
 var urbanDictionaryKey = "e86PuErIxamshxX1NQoXzPi9BD9Sp1C1SgDjsnrfJoJR5BOft9"
 var urbanDictionaryBaseURL = "mashape-community-urban-dictionary.p.mashape.com"
 
-$(document).ready(function () {
+$(document).ready(function() {
 
   // Initialize Firebase
   var config = {
@@ -17,11 +17,21 @@ $(document).ready(function () {
     messagingSenderId: "1074798895512"
   };
   firebase.initializeApp(config);
+  //ajax button click function
 
-  $("#button1").on("click", function () {
+  //clear button function
+  $("#button2").on("click", function clear(event) {
     event.preventDefault();
 
+    $("#data1").empty();
+    $("#data2").empty();
 
+  });
+
+  $("#button1").on("click", function search(event) {
+    event.preventDefault();
+
+    //getting value from input and trimming end spaces
     var term = $("#term").val().trim();
     console.log(term);
     $.ajax({
@@ -33,11 +43,11 @@ $(document).ready(function () {
       },
       url: "https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/inflections/en/" + term,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       console.log(term);
       console.log(response);
       console.log(response.results[0].lexicalEntries[0].inflectionOf[0].text);
-     var rootTerm = response.results[0].lexicalEntries[0].inflectionOf[0].text;
+      var rootTerm = response.results[0].lexicalEntries[0].inflectionOf[0].text;
 
       $.ajax({
 
@@ -48,7 +58,7 @@ $(document).ready(function () {
         },
         url: "https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/entries/en/" + rootTerm,
         method: "GET"
-      }).then(function (response) {
+      }).then(function(response) {
         console.log(term);
         console.log(response);
         console.log(response.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
@@ -57,27 +67,22 @@ $(document).ready(function () {
         $("#tableBody > tbody").prepend("<tr><td>" + term + "</td><td>" + oxDef + "</td><td>");
       })
     });
-      //var term = $("#term").val().trim();
+
+    console.log(term);
+    $.ajax({
+      url: "https://api.urbandictionary.com/v0/define?term=" + term,
+      method: "GET"
+    }).then(function(response) {
       console.log(term);
-      $.ajax({
-        url: "https://api.urbandictionary.com/v0/define?term=" + term,
-        method: "GET"
-      }).then(function (response) {
-        console.log(term);
-        console.log(response);
-        console.log(response.list[0].definition);
-        var urbDef = response.list[0].definition;
-        //urban table write to dom
-        $("#tableBody2 > tbody").prepend("<tr><td>" + term + "</td><td>" + urbDef + "</td><td>");
-      });
-
-
-      $("#term").val("");
+      console.log(response);
+      console.log(response.list[0].definition);
+      var urbDef = response.list[0].definition;
+      //urban table write to dom
+      $("#tableBody2 > tbody").prepend("<tr><td>" + term + "</td><td>" + urbDef + "</td><td>");
     });
 
-
-
-
-
-    //ready document close function tokens
+    $("#term").val("");
   });
+
+  //ready document close function tokens
+});
