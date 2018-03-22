4063846420bb28628f9a62a7e0e7600d1323ab88
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-03-21 20:26:46 -0400

    user validation/error handling

--- assets/javascript/AAAM.js
+++ assets/javascript/AAAM.js
@@ -7,6 +7,7 @@ var urbanDictionaryBaseURL = "mashape-community-urban-dictionary.p.mashape.com"
 
 $(document).ready(function() {
 
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD33QsSEZD9OzsUQz7B_c7Tet4MxMfixpo",
@@ -27,13 +28,14 @@ $(document).ready(function() {
     $("#data2").empty();
 
   });
-
+  var term;
   $("#button1").on("click", function search(event) {
     event.preventDefault();
 
     //getting value from input and trimming end spaces
-    var term = $("#term").val().trim();
+    term = $("#term").val().trim();
     console.log(term);
+    //ajax call to oford dictionary to get root word
     $.ajax({
 
       headers: {
@@ -44,11 +46,13 @@ $(document).ready(function() {
       url: "https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/inflections/en/" + term,
       method: "GET"
     }).then(function(response) {
+      console.log('oxford reposne')
       console.log(term);
       console.log(response);
       console.log(response.results[0].lexicalEntries[0].inflectionOf[0].text);
       var rootTerm = response.results[0].lexicalEntries[0].inflectionOf[0].text;
 
+      //ajax call to oxford dictionary
       $.ajax({
 
         headers: {
@@ -59,30 +63,48 @@ $(document).ready(function() {
         url: "https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com/api/v1/entries/en/" + rootTerm,
         method: "GET"
       }).then(function(response) {
+        console.log('have root word response');
+        if (response.status === 404){
+          return $("#tableBody > tbody").prepend("<tr><td>" + term + "</td><td>" + "No Results" + "</td><td>");
+        }
         console.log(term);
         console.log(response);
         console.log(response.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]);
         var oxDef = response.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0];
+       
         //oxford table write to dom
         $("#tableBody > tbody").prepend("<tr><td>" + term + "</td><td>" + oxDef + "</td><td>");
       })
     });
 
     console.log(term);
+    //ajax call to urban dictionary
     $.ajax({
       url: "https://api.urbandictionary.com/v0/define?term=" + term,
       method: "GET"
     }).then(function(response) {
-      console.log(term);
-      console.log(response);
-      console.log(response.list[0].definition);
-      var urbDef = response.list[0].definition;
-      //urban table write to dom
-      $("#tableBody2 > tbody").prepend("<tr><td>" + term + "</td><td>" + urbDef + "</td><td>");
+     
+      if (response.result_type === "no_results"){
+    
+       return $("#tableBody2 > tbody").prepend("<tr><td>" + term + "</td><td>" + "No Results Found" + "</td><td>");
+     };
+       console.log(term);
+       console.log(response);
+       console.log(response.list[0].definition);
+       var urbDef = response.list.length > 0 ? response.list[0].definition: null;
+       //urban table write to dom
+       $("#tableBody2 > tbody").prepend("<tr><td>" + term + "</td><td>" + urbDef + "</td><td>");
+     
     });
-
     $("#term").val("");
   });
+
+  //error handling for oxford 
+  $(document).ajaxError(function(xhr){
+    $("#tableBody > tbody").prepend("<tr><td>" + term + "</td><td>" + "No Results Found" + "</td><td>");
+    
+    $("#term").val("");
+  })
 
   //ready document close function tokens
 });
